0b7f797737cf73f32a166906562b51e87a9c71e5
Leonardo Motta Maia <>
2018-10-10 23:05:22 -0300

    I think I am getting this wrong.
    The second loop should now solve the system properly.
    
     On branch develop
     Changes to be committed:
    	modified:   beam_warming.c

--- beam_warming.c
+++ beam_warming.c
@@ -25,17 +25,6 @@ void beam_warming(t_define p_setup, t_points ** pnts){
     double theta = 1.0;
     double c_ksi = 0.5;
 
-    /* Separate the diagonals. */
-
-    double *** m_lower = alloc_dcube(4,4,jmax-1);
-    double *** m_main  = alloc_dcube(4,4,jmax-1);
-    double *** m_upper = alloc_dcube(4,4,jmax-1);
-
-    /* Separate solution vectors. */
-
-    double ** du_s = alloc_dmatrix(4,jmax-1);
-    double ** srhs = alloc_dmatrix(4,jmax-1);
-
     /* Define the identity matrix. */
 
     double I[4][4];
@@ -44,174 +33,199 @@ void beam_warming(t_define p_setup, t_points ** pnts){
     I[1][0] = 0.0; I[1][1] = 1.0; I[1][2] = 0.0; I[1][3] = 0.0; 
     I[2][0] = 0.0; I[2][1] = 0.0; I[2][2] = 1.0; I[2][3] = 0.0; 
     I[3][0] = 0.0; I[3][1] = 0.0; I[3][2] = 0.0; I[3][3] = 1.0; 
+    
+    /* Allocate the du_s vector. It has to be the same size of the residue. */
 
-    /* Loop through the field and solve the system for all points in eta. */
+    double *** DU_S  = alloc_dcube(imax, jmax, 4);
+    double **  du_si = alloc_dmatrix(imax-1,4);
+    double **  s_rhs = alloc_dmatrix(imax-1,4);
 
-    for (int i = 1; i<imax-1; i++){
+    /* Allocate the three diagonals. */
 
-        /* Do the first direction. */
+    double *** upper = alloc_dcube(4,4,imax-1);
+    double *** maind = alloc_dcube(4,4,imax-1);
+    double *** lower = alloc_dcube(4,4,imax-1);
 
-        for (int j = 1; j<jmax-1; j++){
+    /* Now loop through every line of the mesh. */
 
-            /* Separate the scalar of the system. */
+    for (int j = 1; j<jmax-1; j++){
+        for (int i = 1; i<imax-1; i++){
 
-            double cnts = (pnts[i][j].dt * theta) / (1.0 + c_ksi);
+            /* Estimate the constant. */
 
-            /* Separate the lower diagonal. */
+            double cte_i = (theta*pnts[i][j].dt)/(1.0 + c_ksi);
 
-            for (int ii = 0; ii<nim; ii++)
-                for (int jj = 0; jj<nim; jj++){
-
-                    /* Build the operator. */
-
-                    m_lower[ii][jj][j] = - (cnts*pnts[i-1][j].A_hat[ii][jj]) / 2.0;
-
-                    /* Add dissipation to the main diagonal. */
-
-                    if (ii == jj) m_lower[ii][jj][j] = m_lower[ii][jj][j] + (2.0*p_setup.dissp2) * pnts[i-1][j].J*pnts[i+1][j].q_hat[ii]; 
-                }
-
-            /* Separate the main diagonal. */
+            /* Build the upper diagonal. */
 
             for (int ii = 0; ii<nim; ii++)
                 for (int jj = 0; jj<nim; jj++){
 
-                    /* Build the operator. */
+                    /* Dissipation free operator. */
 
-                    m_main[ii][jj][j] = I[ii][jj];
+                    upper[ii][jj][i] = cte_i * (pnts[i+1][j].A_hat[ii][jj])/2.0;
 
-                    /* Add dissipation to the main diagonal. */
+                    /* Add the dissipative term. */
 
-                    if (ii == jj) m_main[ii][jj][j] = m_main[ii][jj][j] - (2.0*p_setup.dissp2) * (2.0 * pnts[i][j].J*pnts[i][j].q_hat[ii]);
+                    upper[ii][jj][i] = upper[ii][jj][i] + (p_setup.dissp2) * (pnts[i+1][j].J*pnts[i+1][j].q_hat[0]);
                 }
 
-            /* Separate the lower diagonal. */
+            /* Build the main diagonal. */
 
             for (int ii = 0; ii<nim; ii++)
                 for (int jj = 0; jj<nim; jj++){
 
-                    /* Build the operator. */
+                    /* Dissipation free operator. */
 
-                    m_upper[ii][jj][j] =   (cnts*pnts[i+1][j].A_hat[ii][jj]) / 2.0;
+                    maind[ii][jj][i] = cte_i * (I[ii][jj])/2.0;
 
-                    /* Add dissipation to the main diagonal. */
+                    /* Add the dissipative term. */
 
-                    if (ii == jj) m_upper[ii][jj][j] = m_upper[ii][jj][j] + (2.0*p_setup.dissp2) * (pnts[i+1][j].J*pnts[i-1][j].q_hat[ii]);
+                    maind[ii][jj][i] = maind[ii][jj][i] + (p_setup.dissp2) * - (2.0 * pnts[i][j].J*pnts[i][j].q_hat[0]);
                 }
 
-            /* Separate the srhs. */
-
-            srhs[0][j] = pnts[i][j].RHS[0];
-            srhs[1][j] = pnts[i][j].RHS[1];
-            srhs[2][j] = pnts[i][j].RHS[2];
-            srhs[3][j] = pnts[i][j].RHS[3];
-        }
-
-        /* Solve the system. */
-
-        blk_tri(m_main, m_lower, m_upper, 4, jmax-1, srhs, du_s);
-
-        /* Do the other direction. */
-
-        for (int j = 1; j<jmax-1; j++){
-
-            /* Separate the scalar of the system. */
-
-            double cnts = (pnts[i][j].dt * theta) / (1.0 + c_ksi);
-
-            /* Separate the lower diagonal. */
+            /* Build the lower diagonal. */
 
             for (int ii = 0; ii<nim; ii++)
                 for (int jj = 0; jj<nim; jj++){
 
-                    /* Build the operator. */
+                    /* Dissipation free operator. */
 
-                    m_lower[ii][jj][j] = - (cnts*pnts[i][j-1].B_hat[ii][jj]) / 2.0;
+                    lower[ii][jj][i] = cte_i * (pnts[i-1][j].A_hat[ii][jj])/2.0;
 
-                    /* Add dissipation. */
+                    /* Add the dissipative term. */
 
-                    if (ii == jj) m_lower[ii][jj][j] = m_lower[ii][jj][j] + (2.0*p_setup.dissp2) * (pnts[i][j-1].J*pnts[i][j-1].q_hat[ii]);
+                    lower[ii][jj][i] = lower[ii][jj][i] + (p_setup.dissp2) * (pnts[i-1][j].J*pnts[i-1][j].q_hat[0]);
                 }
 
-            /* Separate the main diagonal. */
+            /* Build the B vector using the RHS. */
 
-            for (int ii = 0; ii<nim; ii++)
-                for (int jj = 0; jj<nim; jj++){
-
-                    /* Build the operator. */
-
-                    m_main[ii][jj][j] = I[ii][jj];
-
-                    /* Add dissipation. */
-
-                    if (ii == jj) m_main[ii][jj][j] = m_main[ii][jj][j] - (2.0*p_setup.dissp2) * (2.0 * pnts[i][j].J*pnts[i][j].q_hat[ii]);
-                }
-
-            /* Separate the lower diagonal. */
-
-            for (int ii = 0; ii<nim; ii++)
-                for (int jj = 0; jj<nim; jj++){
-
-                    /* Build the operator. */
-
-                    m_upper[ii][jj][j] = (cnts*pnts[i][j+1].B_hat[ii][jj]) / 2.0;
-
-                    /* Add dissipation. */
-
-                    if (ii == jj) m_upper[ii][jj][j] = m_upper[ii][jj][j] + (2.0*p_setup.dissp2) * (pnts[i][j+1].J*pnts[i][j+1].q_hat[ii]);
-                }
-
-            /* Separate the srhs. */
-
-            srhs[0][j] = du_s[0][j];
-            srhs[1][j] = du_s[1][j];
-            srhs[2][j] = du_s[2][j];
-            srhs[3][j] = du_s[3][j];
-        }
-
-        /* Solve the system. */
-
-        blk_tri(m_main, m_lower, m_upper, 4, jmax-1, srhs, du_s);
-
-        /* Update the solution. */
-
-        for (int j = 1; j<jmax-1; j++){
-
-            pnts[i][j].q_hat[0] = pnts[i][j].J1 * ( (pnts[i][j].J * pnts[i][j].q_hat[0]) + du_s[0][j] ); 
-            pnts[i][j].q_hat[1] = pnts[i][j].J1 * ( (pnts[i][j].J * pnts[i][j].q_hat[1]) + du_s[1][j] ); 
-            pnts[i][j].q_hat[2] = pnts[i][j].J1 * ( (pnts[i][j].J * pnts[i][j].q_hat[2]) + du_s[2][j] ); 
-            pnts[i][j].q_hat[3] = pnts[i][j].J1 * ( (pnts[i][j].J * pnts[i][j].q_hat[3]) + du_s[3][j] ); 
+            s_rhs[i][0] = pnts[i][j].RHS[0];
+            s_rhs[i][1] = pnts[i][j].RHS[1];
+            s_rhs[i][2] = pnts[i][j].RHS[2];
+            s_rhs[i][3] = pnts[i][j].RHS[3];
 
         }
 
-        /* Clean the solution vectors. */
+        /* Now, solve the system and store the du_s. */
 
-        for (int j = 1; j<jmax-1; j++){
+        blk_tri(maind, lower, upper, nim, imax-1, s_rhs, du_si);
 
-            /* Clean the du_s */
+        /* Store the final DU_S. */
 
-            du_s[0][j] = 0.0;
-            du_s[1][j] = 0.0;
-            du_s[2][j] = 0.0;
-            du_s[3][j] = 0.0;
+        for (int i = 1; i<imax-1; i++){
 
-            /* Clean the srhs */
+            DU_S[i][j][0] = du_si[i][0];
+            DU_S[i][j][1] = du_si[i][1];
+            DU_S[i][j][2] = du_si[i][2];
+            DU_S[i][j][3] = du_si[i][3];
 
-            srhs[0][j] = 0.0;
-            srhs[1][j] = 0.0;
-            srhs[2][j] = 0.0;
-            srhs[3][j] = 0.0;
         }
     }
 
-    /* Free the vectors. */
+    /* Now, free the space used in the i pass. */
 
-    free_dcube(m_lower,4,4);
-    free_dcube(m_main,4,4);
-    free_dcube(m_upper,4,4);
+    free_dmatrix(du_si, imax-1);
+    free_dmatrix(s_rhs, imax-1);
+    free_dcube(upper, 4, 4);
+    free_dcube(maind, 4, 4);
+    free_dcube(lower, 4, 4);
 
-    /* Separate solution vectors. */
+    /* Allocate again every needed guy with proper dimension. */
 
-    free_dmatrix(du_s,4);
-    free_dmatrix(srhs,4);
+    double **  du_sj = alloc_dmatrix(jmax-1,4);
+    double **  s_rhj = alloc_dmatrix(jmax-1,4);
+
+    /* Allocate the three diagonals. */
+
+    double *** jupper = alloc_dcube(4,4,jmax-1);
+    double *** jmaind = alloc_dcube(4,4,jmax-1);
+    double *** jlower = alloc_dcube(4,4,jmax-1);
+
+    /* Now loop through every column of the mesh. */
+
+    for (int i = 1; i<imax-1; i++){
+        for (int j = 1; j<jmax-1; j++){
+
+            /* Estimate the constant. */
+
+            double cte_i = (theta*pnts[i][j].dt)/(1.0 + c_ksi);
+
+            /* Build the upper diagonal. */
+
+            for (int ii = 0; ii<nim; ii++)
+                for (int jj = 0; jj<nim; jj++){
+
+                    /* Dissipation free operator. */
+
+                    jupper[ii][jj][j] = cte_i * (pnts[i][j+1].A_hat[ii][jj])/2.0;
+
+                    /* Add the dissipative term. */
+
+                    jupper[ii][jj][j] = jupper[ii][jj][i] + (p_setup.dissp2) * (pnts[i][j+1].J*pnts[i][j+1].q_hat[0]);
+                }
+
+            /* Build the main diagonal. */
+
+            for (int ii = 0; ii<nim; ii++)
+                for (int jj = 0; jj<nim; jj++){
+
+                    /* Dissipation free operator. */
+
+                    jmaind[ii][jj][j] = cte_i * (I[ii][jj])/2.0;
+
+                    /* Add the dissipative term. */
+
+                    jmaind[ii][jj][j] = jmaind[ii][jj][j] + (p_setup.dissp2) * - (2.0 * pnts[i][j].J*pnts[i][j].q_hat[0]);
+                }
+
+            /* Build the lower diagonal. */
+
+            for (int ii = 0; ii<nim; ii++)
+                for (int jj = 0; jj<nim; jj++){
+
+                    /* Dissipation free operator. */
+
+                    jlower[ii][jj][j] = cte_i * (pnts[i][j-1].A_hat[ii][jj])/2.0;
+
+                    /* Add the dissipative term. */
+
+                    jlower[ii][jj][j] = lower[ii][jj][i] + (p_setup.dissp2) * (pnts[i][j-1].J*pnts[i][j-1].q_hat[0]);
+                }
+
+            /* Build the B vector using the RHS. */
+
+            s_rhj[j][0] = pnts[i][j].RHS[0];
+            s_rhj[j][1] = pnts[i][j].RHS[1];
+            s_rhj[j][2] = pnts[i][j].RHS[2];
+            s_rhj[j][3] = pnts[i][j].RHS[3];
+
+            }
+
+        /* Now, solve the system and store the du_s. */
+
+        blk_tri(jmaind, jlower, jupper, nim, jmax-1, s_rhj, du_sj);
+
+        /* Store the final DU_S. */
+
+        for (int j = 1; j<jmax-1; j++){
+
+            DU_S[i][j][0] = du_sj[j][0];
+            DU_S[i][j][1] = du_sj[j][1];
+            DU_S[i][j][2] = du_sj[j][2];
+            DU_S[i][j][3] = du_sj[j][3];
+        }
+    }
+
+    /* Do not forget the last vector allocated. */
+
+    free_dcube(DU_S, imax, jmax);
+
+    /* Free every auxiliary array. */
+
+    free_dmatrix(du_sj, jmax-1);
+    free_dmatrix(s_rhs, jmax-1);
+    free_dcube(upper, 4, 4);
+    free_dcube(maind, 4, 4);
+    free_dcube(lower, 4, 4);
+
 }
