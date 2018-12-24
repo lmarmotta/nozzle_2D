d9b2ef6d430bfb336b91c2eba4f04ed6655a702d
Leonardo Motta Maia <>
2018-12-24 17:55:06 -0200

    Making some tests with the first order operator.
     On branch develop
     Your branch is up to date with 'origin/develop'.
    
     Changes to be committed:
    	modified:   input.in
    	modified:   rhs_steger_warming.c

--- rhs_steger_warming.c
+++ rhs_steger_warming.c
@@ -45,10 +45,14 @@ void compute_sw_fluxes(t_define p_setup, t_points ** pnts){
 
             /* Compute the auxiliar and the needed variables to compute the fluxes. */
 
+            /* Eq. B5 of SW original paper. */
+
             eig[0] = kt1*u + kt2*v;
             eig[1] = kt1*u + kt2*v;
             eig[2] = kt1*u + kt2*v + a*pow(kt1*kt1 + kt2*kt2,0.5);
             eig[3] = kt1*u + kt2*v - a*pow(kt1*kt1 + kt2*kt2,0.5);
+
+            /* Eq. 4.4 of SW original paper. */
 
             eig_p[0] = (eig[0] + fabs(eig[0])) / 2.0;
             eig_p[1] = (eig[1] + fabs(eig[1])) / 2.0;
@@ -165,7 +169,7 @@ void compute_sw_fluxes(t_define p_setup, t_points ** pnts){
 
 /* Computes the steger warming residues. */
 
-void compute_sw_residue(t_define p_setup, t_points ** pnts){
+void compute_sw_residue_1sto(t_define p_setup, t_points ** pnts){
 
     /* Separate bounds of the field. */
 
@@ -186,6 +190,151 @@ void compute_sw_residue(t_define p_setup, t_points ** pnts){
 
     double dbGp_eta[4];
     double dfGm_eta[4];
+
+    /* Initialize the residues to avoid problems. */
+
+    for (int i = 0; i<imax; i++){
+        for (int j = 0; j<jmax; j++){
+
+            pnts[i][j].RHS[0] = -10.0;
+            pnts[i][j].RHS[1] = -10.0;
+            pnts[i][j].RHS[2] = -10.0;
+            pnts[i][j].RHS[3] = -10.0;
+        }
+    }
+
+    /* Build the fluxes for real. */
+
+    for (int i = 1; i<imax-1; i++){
+        for (int j = 1; j<jmax-1; j++){
+
+            /* F flux in ksi direction */
+
+            dbFp_ksi[0] = pnts[i][j].f_plus[0] - pnts[i-1][j].f_plus[0];
+            dbFp_ksi[1] = pnts[i][j].f_plus[1] - pnts[i-1][j].f_plus[1];
+            dbFp_ksi[2] = pnts[i][j].f_plus[2] - pnts[i-1][j].f_plus[2];
+            dbFp_ksi[3] = pnts[i][j].f_plus[3] - pnts[i-1][j].f_plus[3];
+
+            dfFm_ksi[0] = pnts[i+1][j].f_minu[0] - pnts[i][j].f_minu[0];
+            dfFm_ksi[1] = pnts[i+1][j].f_minu[1] - pnts[i][j].f_minu[1];
+            dfFm_ksi[2] = pnts[i+1][j].f_minu[2] - pnts[i][j].f_minu[2];
+            dfFm_ksi[3] = pnts[i+1][j].f_minu[3] - pnts[i][j].f_minu[3];
+
+            /* G flux in eta direction */
+
+            dbGp_eta[0] = pnts[i][j].g_plus[0] - pnts[i][j-1].g_plus[0];
+            dbGp_eta[1] = pnts[i][j].g_plus[1] - pnts[i][j-1].g_plus[1];
+            dbGp_eta[2] = pnts[i][j].g_plus[2] - pnts[i][j-1].g_plus[2];
+            dbGp_eta[3] = pnts[i][j].g_plus[3] - pnts[i][j-1].g_plus[3];
+
+            dfGm_eta[0] = pnts[i][j+1].g_minu[0] - pnts[i][j].g_minu[0];
+            dfGm_eta[1] = pnts[i][j+1].g_minu[1] - pnts[i][j].g_minu[1];
+            dfGm_eta[2] = pnts[i][j+1].g_minu[2] - pnts[i][j].g_minu[2];
+            dfGm_eta[3] = pnts[i][j+1].g_minu[3] - pnts[i][j].g_minu[3];
+
+            /* Build the residues. */
+
+            pnts[i][j].RHS[0] = dbFp_ksi[0] + dfFm_ksi[0] + dbGp_eta[0] + dfGm_eta[0];
+            pnts[i][j].RHS[1] = dbFp_ksi[1] + dfFm_ksi[1] + dbGp_eta[1] + dfGm_eta[1];
+            pnts[i][j].RHS[2] = dbFp_ksi[2] + dfFm_ksi[2] + dbGp_eta[2] + dfGm_eta[2];
+            pnts[i][j].RHS[3] = dbFp_ksi[3] + dfFm_ksi[3] + dbGp_eta[3] + dfGm_eta[3];
+
+            
+        }
+    }
+
+    /* Collect the maximun residue. */
+
+    for (int i = 1; i<imax-1; i++){
+        for (int j = 1; j<jmax-1; j++){
+
+            /* Store the max residue. */
+
+            if (fabs( pnts[i][j].RHS[0]) > max_rhs_rho ) max_rhs_rho  = fabs(pnts[i][j].RHS[0]+DBL_EPSILON);
+            if (fabs( pnts[i][j].RHS[1]) > max_rhs_rhou) max_rhs_rhou = fabs(pnts[i][j].RHS[1]+DBL_EPSILON);
+            if (fabs( pnts[i][j].RHS[2]) > max_rhs_rhov) max_rhs_rhov = fabs(pnts[i][j].RHS[2]+DBL_EPSILON);
+            if (fabs( pnts[i][j].RHS[3]) > max_rhs_e   ) max_rhs_e    = fabs(pnts[i][j].RHS[3]+DBL_EPSILON);
+        }
+    }
+}
+
+/* Computes the steger warming residues. */
+
+void compute_sw_residue_2ndo(t_define p_setup, t_points ** pnts){
+
+    /* Separate bounds of the field. */
+
+    int imax = p_setup.imax;
+    int jmax = p_setup.jmax;
+
+    /* Initialize the residue. */
+
+    max_rhs_rho  = -99999999.0;
+    max_rhs_rhou = -99999999.0;
+    max_rhs_rhov = -99999999.0;
+    max_rhs_e    = -99999999.0;
+
+    /* Now, with the splited fluxes computed, lets join everything in our residue. For internal points */
+
+    double dbFp_ksi[4];
+    double dfFm_ksi[4];
+
+    double dbGp_eta[4];
+    double dfGm_eta[4];
+
+    /* Initialize the residues to avoid problems. */
+
+    for (int i = 0; i<imax; i++){
+        for (int j = 0; j<jmax; j++){
+
+            pnts[i][j].RHS[0] = -10.0;
+            pnts[i][j].RHS[1] = -10.0;
+            pnts[i][j].RHS[2] = -10.0;
+            pnts[i][j].RHS[3] = -10.0;
+        }
+    }
+
+    /* Build the fluxes for real. */
+
+    for (int i = 1; i<imax-1; i++){
+        for (int j = 1; j<jmax-1; j++){
+
+            /* F flux in ksi direction */
+
+            dbFp_ksi[0] = pnts[i][j].f_plus[0] - pnts[i-1][j].f_plus[0];
+            dbFp_ksi[1] = pnts[i][j].f_plus[1] - pnts[i-1][j].f_plus[1];
+            dbFp_ksi[2] = pnts[i][j].f_plus[2] - pnts[i-1][j].f_plus[2];
+            dbFp_ksi[3] = pnts[i][j].f_plus[3] - pnts[i-1][j].f_plus[3];
+
+            dfFm_ksi[0] = pnts[i+1][j].f_minu[0] - pnts[i][j].f_minu[0];
+            dfFm_ksi[1] = pnts[i+1][j].f_minu[1] - pnts[i][j].f_minu[1];
+            dfFm_ksi[2] = pnts[i+1][j].f_minu[2] - pnts[i][j].f_minu[2];
+            dfFm_ksi[3] = pnts[i+1][j].f_minu[3] - pnts[i][j].f_minu[3];
+
+            /* G flux in eta direction */
+
+            dbGp_eta[0] = pnts[i][j].g_plus[0] - pnts[i][j-1].g_plus[0];
+            dbGp_eta[1] = pnts[i][j].g_plus[1] - pnts[i][j-1].g_plus[1];
+            dbGp_eta[2] = pnts[i][j].g_plus[2] - pnts[i][j-1].g_plus[2];
+            dbGp_eta[3] = pnts[i][j].g_plus[3] - pnts[i][j-1].g_plus[3];
+
+            dfGm_eta[0] = pnts[i][j+1].g_minu[0] - pnts[i][j].g_minu[0];
+            dfGm_eta[1] = pnts[i][j+1].g_minu[1] - pnts[i][j].g_minu[1];
+            dfGm_eta[2] = pnts[i][j+1].g_minu[2] - pnts[i][j].g_minu[2];
+            dfGm_eta[3] = pnts[i][j+1].g_minu[3] - pnts[i][j].g_minu[3];
+
+            /* Build the residues. */
+
+            pnts[i][j].RHS[0] = dbFp_ksi[0] + dfFm_ksi[0] + dbGp_eta[0] + dfGm_eta[0];
+            pnts[i][j].RHS[1] = dbFp_ksi[1] + dfFm_ksi[1] + dbGp_eta[1] + dfGm_eta[1];
+            pnts[i][j].RHS[2] = dbFp_ksi[2] + dfFm_ksi[2] + dbGp_eta[2] + dfGm_eta[2];
+            pnts[i][j].RHS[3] = dbFp_ksi[3] + dfFm_ksi[3] + dbGp_eta[3] + dfGm_eta[3];
+
+            
+        }
+    }
+
+    /* Build the fluxes for real. */
 
     for (int i = 2; i<imax-2; i++){
         for (int j = 2; j<jmax-2; j++){
@@ -503,5 +652,3 @@ void compute_sw_residue(t_define p_setup, t_points ** pnts){
         }
     }
 }
-
-
